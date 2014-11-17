fac52bc484a8545b03d52b262846b7fb108fc215
denizoztreves <doztreves@ku.edu.tr>
2014-11-17 12:12:03 +0200

    Excelden okunan Instructor DB ye ekleniyor. Ve login islemi artik yapilabiliyor

--- taas/src/Helper/ExcelHelper.java
+++ taas/src/Helper/ExcelHelper.java
@@ -19,67 +19,93 @@ import Model.Instructor;
 public class ExcelHelper {
 	static String chosenFile = "TAAS EXCEL.xlsx";
 
+
 	public static void main(String []args){
 		/**
 		 * Gets the excel and parses it
 		 * @param choosenfile
 		 */
+
+
 		//public void getDataFromExcelFile(String chosenFile){
-			try {
-				FileInputStream file = new FileInputStream(chosenFile);
-				XSSFWorkbook workbook = new XSSFWorkbook(file);
-
-				XSSFSheet coursesExcel = workbook.getSheet("Courses");
-				XSSFSheet instructorExcel = workbook.getSheet("Instructors");
-				XSSFSheet assistantExcel = workbook.getSheet("Assistants");
-
-				Iterator<Row> coursesRowIterator = coursesExcel.iterator();
-				Iterator<Row> instructorRowIterator = instructorExcel.iterator();
-				Iterator<Row> assistantRowIterator = assistantExcel.iterator();
+		try {
+			FileInputStream file = new FileInputStream(chosenFile);
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
 
 
-				Row bosaRow = instructorRowIterator.next();
-				while(instructorRowIterator.hasNext()){
+			XSSFSheet instructorExcel = workbook.getSheet("Instructors");
+			XSSFSheet assistantExcel = workbook.getSheet("Assistants");
 
-					Row row = instructorRowIterator.next();
+			ExcelHelper.getInstructorsFromWorksheet(instructorExcel);
 
-					Cell nameCell = row.getCell(0);
-					Cell surnameCell = row.getCell(1);
-					Cell mailCell = row.getCell(2);
-					Cell deptCell = row.getCell(3);
-
-
-					String name = nameCell.getStringCellValue();
-					String surname = surnameCell.getStringCellValue();
-					String mail = mailCell.getStringCellValue();
-					String dept = deptCell.getStringCellValue();
-					
-					Department d = new Department(dept);
-					Instructor ins = new Instructor(name, surname, mail, d);
-
-				}
-				
-				Row dummyRow = coursesRowIterator.next();
-				while(coursesRowIterator.hasNext()){
-					
-					Row row = coursesRowIterator.next();
-					
-					Cell codeCell = row.getCell(0);
-					Cell numberCell = row.getCell(1);
-					Cell capacityCell = row.getCell(2);
-					
-					String code = codeCell.getStringCellValue();
-					int number = (int) numberCell.getNumericCellValue();
-					int capacity = (int) capacityCell.getNumericCellValue();
-					
-					Course course = new Course(code, number, capacity);
-				}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
 
 
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
+
+	private void getCoursesFromWorksheet(XSSFWorkbook workbook) {
+		XSSFSheet coursesExcel = workbook.getSheet("Courses");
+		Iterator<Row> coursesRowIterator = coursesExcel.iterator();
+		Row dummyRow = coursesRowIterator.next();
+		while(coursesRowIterator.hasNext()){
+
+			Row row = coursesRowIterator.next();
+
+			Cell codeCell = row.getCell(0);
+			Cell numberCell = row.getCell(1);
+			Cell capacityCell = row.getCell(2);
+
+			String code = codeCell.getStringCellValue();
+			int number = (int) numberCell.getNumericCellValue();
+			int capacity = (int) capacityCell.getNumericCellValue();
+
+			Course course = new Course(code, number, capacity);
 		}
 	}
+
+	public static void getInstructorsFromWorksheet(XSSFSheet instructorExcel){
+
+		Iterator<Row> instructorRowIterator = instructorExcel.iterator();
+
+
+		Row dummyRow = instructorRowIterator.next();
+		while(instructorRowIterator.hasNext()){
+
+			Row row = instructorRowIterator.next();
+
+			
+				Cell nameCell = row.getCell(0);
+				Cell surnameCell = row.getCell(1);
+				Cell mailCell = row.getCell(2);
+				Cell deptCell = row.getCell(3);
+
+				
+				String name = nameCell.getStringCellValue();
+				String surname = surnameCell.getStringCellValue();
+				String mail = mailCell.getStringCellValue();
+				String dept = deptCell.getStringCellValue();
+
+				if(!(name.isEmpty() || surname.isEmpty() || mail.isEmpty() || dept.isEmpty() )){
+				Department d = new Department(dept);
+				Instructor ins = new Instructor(name, surname, mail, d);
+				if(!ins.checkInstructorIsInDB()){ // Instructor is not in the database. Add it.
+					//createRandomPassword 
+					MailHelper mh = new MailHelper();
+					String rp =  mh.generateRandomPassword();
+					ins.addToDB(rp);
+					mh.sendEmail(ins,rp);
+				}
+			}else
+			{
+				System.out.println("Error on entry. \nName : "+name+"\nSurname : "+surname+"\nMail : "+mail+"\nDepartment_ID : "+dept);
+				System.out.println();
+			}
+		}
+
+	}
+
+}
 //}
--- taas/src/View/InstructorView.java
+++ taas/src/View/InstructorView.java
@@ -7,22 +7,43 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import Model.Instructor;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+
 public class InstructorView extends JFrame {
 
 	private JPanel contentPane;
-
+	private Instructor instructor;
 	
 
 	/**
 	 * Create the frame.
 	 */
-	public InstructorView() {
+	public InstructorView(Instructor inst) {
+		
+		instructor = inst;
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblIdentification = new JLabel("New label");
+		lblIdentification.setHorizontalAlignment(SwingConstants.CENTER);
+		lblIdentification.setBounds(266, 21, 178, 16);
+		lblIdentification.setText(instructor.name + " " + instructor.surname );
+		contentPane.add(lblIdentification);
+		
+		JLabel lblCourses = new JLabel("Courses");
+		lblCourses.setBounds(28, 53, 72, 16);
+		contentPane.add(lblCourses);
+		
+		JComboBox cbTeaching = new JComboBox();
+		cbTeaching.setBounds(112, 49, 178, 27);
+		contentPane.add(cbTeaching);
 	}
-
 }
