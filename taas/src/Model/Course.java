72ae5a367633395df337f6f7e2b27c5dcd060aa1
denizoztreves <doztreves@ku.edu.tr>
2014-11-30 16:00:11 +0200

    course id handling

--- taas/src/Model/Course.java
+++ taas/src/Model/Course.java
@@ -4,12 +4,12 @@ import Helper.DatabaseHelper;
 
 public class Course {
 
-
+	public int id;
 	public String code;
 	public int number;
 	public int capacity;
 	public int maxAssistantNumber;
-	
+	public String instrReq;
 	DatabaseHelper dbh; 
 
 	public Course(String code, int num){
@@ -17,17 +17,29 @@ public class Course {
 		number = num;
 		dbh = new DatabaseHelper();
 		capacity = getCapacityFromDB();
+		setID();
 		setMaxAssistantNumber(capacity);
+		updateMaxAssistantCount();
 	}
 	public Course(String c, int n, int capacity) { 
 		code = c;
 		number = n;
 		this.capacity = capacity;
+		setID();
 		setMaxAssistantNumber(capacity);
+		updateMaxAssistantCount();
 		dbh = new DatabaseHelper();
 	}
 
 
+	public Course(String code, int number, int capacity, int maxasst) {
+		// TODO Auto-generated constructor stub
+		this.code = code;
+		this.number = number;
+		this.capacity = capacity;
+		this.maxAssistantNumber = maxasst;
+		
+	}
 	private void setMaxAssistantNumber(int c) {
 
 		double d = c;
@@ -49,4 +61,18 @@ public class Course {
 		// TODO Auto-generated method stub
 		return this.code + " " + this.number + " [Max Assistant request: "+maxAssistantNumber +"]" ;
 	}
+	public void setID() {
+		// TODO Auto-generated method stub
+		id = dbh.getCourseID(this);
+	}
+	public void updateMaxAssistantCount() {
+		// TODO get max asst count subtract request number
+		
+		int max = this.maxAssistantNumber;
+		int reqCount = dbh.getRequestCountForCourse(this);
+		this.maxAssistantNumber = max - reqCount;
+		
+	}
+	
+	//sadsadsa
 }
